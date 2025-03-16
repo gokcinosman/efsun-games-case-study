169700d37fb112c587c195ec35e3c6bb85ccf42d
Gokcin Osman <osmangokcinn@gmail.com>
2025-03-16 14:33:23 +0300

    Created scene basics and hayfactory

--- Assets/MiniFarm/Scripts/Factories/BaseFactory.cs
+++ Assets/MiniFarm/Scripts/Factories/BaseFactory.cs
@@ -1,14 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 public class BaseFactory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] protected int capacity;
+    [SerializeField] protected float productionTime;
+    protected int currentStock = 0;
+    private IDisposable productionCoroutine; // UniRX timer
+    public IObservable<int> OnStockChanged => stockSubject;
+    private Subject<int> stockSubject = new Subject<int>();
+    protected virtual void Start()
     {
+        stockSubject.OnNext(currentStock);
     }
-    // Update is called once per frame
-    void Update()
+    public void StartProduction()
     {
+        if (currentStock >= capacity) return;
+        productionCoroutine = Observable.Timer(TimeSpan.FromSeconds(productionTime)).Repeat().Subscribe(_ =>
+        {
+            ProduceItem();
+        });
+    }
+    private void ProduceItem()
+    {
+        if (currentStock < capacity)
+        {
+            currentStock++;
+            stockSubject.OnNext(currentStock);
+        }
+    }
+    public int CollectItems()
+    {
+        int collected = currentStock;
+        currentStock = 0;
+        stockSubject.OnNext(currentStock);
+        return collected;
+    }
+    protected virtual void OnDestroy()
+    {
+        productionCoroutine?.Dispose(); // UniRx timer dispose
     }
 }
--- Assets/MiniFarm/Scripts/Factories/HayFactory.cs
+++ Assets/MiniFarm/Scripts/Factories/HayFactory.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class HayFactory : MonoBehaviour
+public class HayFactory : BaseFactory
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        base.Start();
+        StartProduction();
     }
 }
