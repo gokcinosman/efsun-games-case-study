e5d0b3708bf4707a7f34b422bcc818e55ffb0231
Gokcin Osman <osmangokcinn@gmail.com>
2025-03-17 22:23:18 +0300

    uipositioner optimized by unirx

--- Assets/MiniFarm/Scripts/UI/UIPositioner.cs
+++ Assets/MiniFarm/Scripts/UI/UIPositioner.cs
@@ -1,22 +1,44 @@
+using System;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
+using Zenject;
 public class UIPositioner : MonoBehaviour
 {
     private Vector3 offset = new Vector3(0, -2f, 0);
-    private Transform targetTransform;
+    private ReactiveProperty<Transform> targetTransform = new ReactiveProperty<Transform>();
+    private CompositeDisposable disposables = new CompositeDisposable();
+    private Camera mainCamera;
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        this.LateUpdateAsObservable()
+            .Subscribe(_ => UpdatePositionAndRotation())
+            .AddTo(disposables);
+        targetTransform
+            .Where(target => target != null)
+            .Subscribe(_ => UpdatePositionAndRotation())
+            .AddTo(disposables);
+    }
     public void SetTarget(Transform target)
     {
-        targetTransform = target;
+        targetTransform.Value = target;
     }
     public void SetOffset(Vector3 newOffset)
     {
         offset = newOffset;
+        UpdatePositionAndRotation();
     }
-    private void LateUpdate()
+    private void UpdatePositionAndRotation()
     {
-        if (targetTransform != null)
+        if (targetTransform.Value != null)
         {
-            transform.position = targetTransform.position + offset;
-            transform.rotation = Camera.main.transform.rotation;
+            transform.position = targetTransform.Value.position + offset;
+            transform.rotation = mainCamera.transform.rotation;
         }
+    }
+    private void OnDestroy()
+    {
+        disposables.Dispose();
     }
 }
